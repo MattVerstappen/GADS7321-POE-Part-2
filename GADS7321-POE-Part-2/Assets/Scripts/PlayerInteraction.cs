49e6cfa357109022cfcfc385ea9bd0490aeb9f51
Wesley Clark <wesley@studiosnark.co.za>
2024-05-24 14:58:03 +0200

    Added INK Dialogue system to Assets package.
    Added iNK in preperation for dialogue system for NPC's.
    
    REFER TO THIS VIDEO TUTORIAL:
    https://www.youtube.com/watch?v=vY0Sk93YUhA

--- GADS7321-POE-Part-2/Assets/Scripts/PlayerInteraction.cs
+++ GADS7321-POE-Part-2/Assets/Scripts/PlayerInteraction.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    [SerializeField] private float interactionRadius = 1.5f; // Interaction radius around the player
+    [SerializeField] private float interactionRadius = 1f;
     [SerializeField] private LayerMask interactableLayer;
 
     private void Update()
@@ -15,26 +15,20 @@ public class PlayerInteraction : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        // Draw a wire sphere to visualize the interaction radius in the scene view
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, interactionRadius);
     }
 
     private void InteractWithObjects()
     {
-        // Find all colliders within the interaction radius around the player
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, interactionRadius, interactableLayer);
 
-        // Loop through all detected colliders
         foreach (Collider2D collider in colliders)
         {
             Interactable interactable = collider.GetComponent<Interactable>();
             if (interactable != null)
             {
-                // Interact with the object
                 interactable.Interact();
-
-                // Display interaction feedback in the console
                 Debug.Log("Interacted with: " + interactable.name);
             }
         }
